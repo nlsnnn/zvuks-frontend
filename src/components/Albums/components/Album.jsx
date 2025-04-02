e92f63f5f74de98ed24c2d906824c49a02170600
nlsnnn <egor.larrr@gmail.com>
2025-04-02 16:03:48 +0300

    add favorite album adding

--- src/components/Albums/components/Album.jsx
+++ src/components/Albums/components/Album.jsx
@@ -3,6 +3,9 @@ import { albumStore } from "../../../store/albumStore";
 import { observer } from "mobx-react-lite";
 import { AlbumSongs } from "../../AlbumSongs/AlbumSongs";
 import { Header } from "../../Header/Header";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { favoriteStore } from "../../../store/favoriteStore"
 
 export const Album = observer(({ albumId }) => {
   useEffect(() => {
@@ -12,6 +15,10 @@ export const Album = observer(({ albumId }) => {
 
   if (!albumStore.album) {
     return <div>Загрузка...</div>;
+  }
+
+  const handleHeartClick = async () => {
+    await favoriteStore.addAlbum(albumId)
   }
 
   return (
@@ -24,7 +31,14 @@ export const Album = observer(({ albumId }) => {
             alt={albumStore.album?.name}
             className="w-48 h-48 rounded object-cover"
           />
-          <h2 className="text-3xl font-bold">{albumStore.album?.name}</h2>
+          <div>
+            <h2 className="text-3xl font-bold">{albumStore.album?.name}</h2>
+            <FontAwesomeIcon
+              icon={faHeart}
+              className="hover:text-red-500 transition cursor-pointer"
+              onClick={handleHeartClick}
+            />
+          </div>
         </div>
         <AlbumSongs albumId={albumId} />
       </main>
