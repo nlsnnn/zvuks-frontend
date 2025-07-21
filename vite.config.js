9154eaf911e9c8a9274fee359f6d9b4cf8b4b2f9
nlsnnn <egor.larrr@gmail.com>
2025-07-21 18:57:20 +0300

    feat: add load env in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react(), tailwindcss()],
+    define: {
+      "import.meta.env.VITE_API_BASE": JSON.stringify(env.VITE_API_BASE),
+    },
+  };
+});
